8597202218acdcbd269fea8f2c1aba21ac4f18b7
priyadarshan-pd <34839992+priyadarshan-pd@users.noreply.github.com>
2018-02-06 00:09:38 +0530

    Update arrayadd.c

--- arrayadd.c
+++ arrayadd.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 int main(void) {
 	int a[10000],n,k,sum;
 	sum=0;
-	scanf("%d %d",&n,&k);
+	scanf("%d",&n);
 	for(int i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
+	scanf("%d",&k);
 	for(int j=0;j<k;j++)
 	{
 		sum=sum+a[j];
